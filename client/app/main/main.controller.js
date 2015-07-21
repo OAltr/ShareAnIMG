112742a90b861f79d16e28cec7c8ed6ba7049898
Oscar <oscar.altrogge@me.com>
2015-07-21 23:36:57 +0200

    Replaced things with imgLinks

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,27 +1,27 @@
 'use strict';
 
 angular.module('shareAnImgApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+	.controller('MainCtrl', function ($scope, $http, Auth, socket) {
+		$scope.allLinks = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
+		$http.get('/api/imgLinks').success(function(allLinks) {
+			$scope.allLinks = allLinks;
+			socket.syncUpdates('imgLink', $scope.allLinks);
+		});
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
+		$scope.addLink = function() {
+			if($scope.newLink === '') {
+				return;
+			}
+			$http.post('/api/imgLinks', { link: $scope.newLink, owner: Auth.getCurrentUser() });
+			$scope.newLink = '';
+		};
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
+		$scope.deleteLink = function(link) {
+			$http.delete('/api/imgLinks/' + link._id);
+		};
 
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
-  });
+		$scope.$on('$destroy', function () {
+			socket.unsyncUpdates('imgLink');
+		});
+	});
