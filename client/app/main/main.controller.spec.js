a5993493dfcfcde61c13a34657f88503c288a8c0
Oscar <oscar.altrogge@me.com>
2015-07-26 23:50:39 +0200

    Updated test for main
    Works just without angulargrid

--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -2,28 +2,31 @@
 
 describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('shareAnImgApp'));
-  beforeEach(module('socketMock'));
+	// load the controller's module
+	beforeEach(module('shareAnImgApp'));
+	beforeEach(module('socketMock'));
 
-  var MainCtrl,
-      scope,
-      $httpBackend;
+	var MainCtrl,
+			scope,
+			$httpBackend;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/things')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+	// Initialize the controller and a mock scope
+	beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+		$httpBackend = _$httpBackend_;
+		$httpBackend.expectGET('/api/imgLinks')
+			.respond([{link: 'http://www.lessons4living.com/images/penclchk.gif'},
+				{link: 'https://blog.optimizely.com/wp-content/uploads/2013/06/Screen-Shot-2013-06-11-at-6.27.42-PM.png'},
+				{link: 'http://touchportal.de/wp-content/uploads/2014/06/The-Test-Fun-for-Friends-von-LOTUM-Apps-150x150.png'},
+				{link: 'http://www.lessons4living.com/images/penclchk.gif'}]);
 
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-    });
-  }));
+		scope = $rootScope.$new();
+		MainCtrl = $controller('MainCtrl', {
+			$scope: scope
+		});
+	}));
 
-  it('should attach a list of things to the scope', function () {
-    $httpBackend.flush();
-    expect(scope.awesomeThings.length).toBe(4);
-  });
+	it('should attach a list of links to the scope', function () {
+		$httpBackend.flush();
+		expect(scope.allLinks.length).toBe(4);
+	});
 });
